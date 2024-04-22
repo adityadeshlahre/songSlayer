39f1d86f13fecb4fb39bd182bb3401c7ae280dc4
Aditya @ArchLinux <132184385+adityadeshlahre@users.noreply.github.com>
2024-04-22 12:42:49 +0530

    react FE init & backend folder restructured

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="">Hello WebSocket</div>
+      <div>
+        <button className="bg-red-300">Create Room</button>
+        <br />
+        <button>Join Room</button>
+        <br />
+        <button>Add Songs Room</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
